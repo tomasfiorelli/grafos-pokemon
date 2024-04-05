9c61fbcc5f3a1d566bd523bf6a35828f1dc24ff8
Tomás Fiorelli Barbosa <tomasfiorellibarbosa@icloud.com>
2024-04-04 22:06:01 -0300

    Menu e funções de 1 a 8

--- grafoMatriz.py
+++ grafoMatriz.py
@@ -1,164 +1,105 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 13 13:59:10 2023
+from collections import deque 
 
-@author: icalc
-"""
 class Grafo:
-    TAM_MAX_DEFAULT = 100 # qtde de vértices máxima default
-    # construtor da classe grafo
-    def __init__(self, n=TAM_MAX_DEFAULT):
-        self.n = n # número de vértices
-        self.m = 0 # número de arestas
-        # matriz de adjacência
-        self.adj = [[0 for i in range(n)] for j in range(n)]
+    def __init__(self):
+        # Dicionário para armazenar os vizinhos e pesos das arestas
+        self.tipo_grafo = 6
+        self.qtde_vertices = 0
+        self.qtde_arestas = 0
+        self.adj = {}
 
-    # Insere uma aresta no Grafo tal que
-    # v é adjacente a w
-    def insereA(self, v, w):
-        if self.adj[v][w] == 0:
-            self.adj[v][w] = 1
-            self.m+=1 # atualiza qtd arestas
+    def insereV(self, v):
+        if v not in self.adj:
+            self.adj[v] = {}
+        self.qtde_vertices += 1
     
-    # remove uma aresta v->w do Grafo	
+    def removeV(self, vertice):
+        if vertice in self.adj:
+            del self.adj[vertice]
+            self.qtde_vertices -= 1
+            for v in self.adj:  # Remove todas as arestas que envolvem o vértice
+                if vertice in self.adj[v]:
+                    del self.adj[v][vertice]
+                    self.qtde_arestas -= 1
+            print(f"Vértice '{vertice}' removido com sucesso e todas as suas arestas relacionadas.")
+        else:
+            print(f"O vértice '{vertice}' não existe no grafo.")
+
+    def insereA(self, v, w, peso=1.0):
+        if v not in self.adj:
+            self.adj[v] = {}
+        if w not in self.adj:
+            self.adj[w] = {}
+        self.adj[v][w] = peso
+        self.qtde_arestas += 1
+
     def removeA(self, v, w):
-        # testa se temos a aresta
-        if self.adj[v][w] == 1:
-            self.adj[v][w] = 0
-            self.m-=1; # atualiza qtd arestas
+        if v in self.adj and w in self.adj[v]:
+            del self.adj[v][w]
+        self.qtde_arestas -= 1
 
-    # Apresenta o Grafo contendo
-    # número de vértices, arestas
-    # e a matriz de adjacência obtida	
     def show(self):
-        print(f"\n n: {self.n:2d} ", end="")
-        print(f"m: {self.m:2d}\n")
-        for i in range(self.n):
-            for w in range(self.n):
-                if self.adj[i][w] == 1:
-                    print(f"Adj[{i:2d},{w:2d}] = 1 ", end="") 
-                else:
-                    print(f"Adj[{i:2d},{w:2d}] = 0 ", end="")
-            print("\n")
-        print("fim da impressao do grafo." )
+        print("\nGRAFO [6] - grafo orientado com peso na aresta")
+        print(f"Quantidade de vértices: {self.qtde_vertices}")
+        print(f"Quantidade de arestas: {self.qtde_arestas}\n")
+        for v, vizinhos in self.adj.items():
+            print(f"{v[:3]}: ", end="")
+            for vizinho, peso in vizinhos.items():
+                print(f"[{vizinho[:3]}] = {peso}", end=" | ")
+            print()
+        print("\nFim da impressao do grafo.\n\n")
 
-    # Apresenta o Grafo contendo
-    # número de vértices, arestas
-    # e a matriz de adjacência obtida 
-    # Apresentando apenas os valores 0 ou 1	
     def showMin(self):
-        print(f"\n n: {self.n:2d} ", end="")
-        print(f"m: {self.m:2d}\n")
-        for i in range(self.n):
-            for w in range(self.n):
-                if self.adj[i][w] == 1:
-                    print(" 1 ", end="") 
+        vertices = self.adj.keys()
+        print("\n === MATRIZ DE ADJACÊNCIA ===")
+        print(" " * 4, end="")
+        for v in vertices:
+            print(f"{v[:3]:^6}", end="")
+        print()
+        for v1 in vertices:
+            print(f"{v1[:3]:<4}", end="")
+            for v2 in vertices:
+                if v2 in self.adj.get(v1, {}):
+                    print(f"{self.adj[v1][v2]:^6.1f}", end="")
                 else:
-                    print(" 0 ", end="")
-            print("\n")
-        print("fim da impressao do grafo." )
-    
-    # --- EXERCÍCIO 1 ---
-    # Função que retorna o grau
-    # de entrada de um vértice v,
-    # total de arestas que chegam
-    def inDegree(self, v):
-        in_degree = 0
-        for i in range(self.n):
-            in_degree += self.adj[i][v]
-        return in_degree
+                    print(f"{'-':^6}", end="")
+            print()
+        print("\nFim da impressao da matriz de adjacência.\n\n")
 
-    # --- EXERCÍCIO 2 ---
-    # Função que retorna o grau
-    # de entrada de um vértice v,
-    # total de arestas que saem
-    def outDegree(self, v):
-        out_degree = 0
-        for j in range(self.n):
-            out_degree += self.adj[v][j]
-        return out_degree
+    def lerArquivo(self, nome_arquivo):
+        with open(nome_arquivo, 'r', encoding="utf-8") as arquivo:
+            self.tipo_grafo = int(arquivo.readline())
+            
+            quantidade_vertices = int(arquivo.readline().strip())
+            for _ in range(quantidade_vertices):
+                self.insereV(arquivo.readline().strip())
+            
+            quantidade_arestas = int(arquivo.readline())
+            for _ in range(quantidade_arestas):
+                origem, destino, peso = arquivo.readline().split()
+                peso_float = float(peso)
+                self.insereA(origem, destino, peso_float)
 
-    # --- EXERCÍCIO 3 ---
-    # Função que retorna se
-    # o vértice v é fonte
-    def source(self, v):
-        return 1 if (self.outDegree(v) > 0 and self.inDegree(v) == 0) else 0 
+    def salvarArquivo(self, nome_arquivo):
+        with open(nome_arquivo, 'w', encoding="utf-8") as arquivo:
+            # Escreve o tipo de grafo
+            arquivo.write("6\n")
 
-    # --- EXERCÍCIO 4 ---
-    # Função que retorna se
-    # o vértice v é sorvedouro
-    def sink(self, v):
-        return 1 if (self.inDegree(v) > 0 and self.outDegree(v) == 0) else 0 
-    
-    # --- EXERCÍCIO 5 ---
-    # Função que retorna se
-    # o grafo g é simétrico
-    def isSymmetric(self):
-        for i in range(self.n):
-            for j in range(self.n):
-                if self.adj[i][j] != self.adj[j][i]:
-                    return 0
-        return 1
-    
-    # --- EXERCÍCIO 6 ---
-    # Leitura do grafo no
-    # arquivo gerado
-    @classmethod
-    def fromFile(cls, filename):
-        try:
-            with open(filename, 'r') as file:
-                lines = file.readlines()
-                num_vertices = int(lines[0].strip())
-                num_edges = int(lines[1].strip())
-                graph = cls(num_vertices)
-                for line in lines[2:]:
-                    v, w = map(int, line.split())
-                    graph.insereA(v, w)
-            print(f"Leitura do arquivo: <{filename}>")
-            # graph.showMin()
-            return graph
-        except:
-            print(f"Falha na leitura do arquivo: <{filename}>")
-            return None
+            # Escreve a quantidade de vértices
+            arquivo.write(f"{len(self.adj)}\n")
 
-# Grafo Não-Direcionado
-class GrafoND:
-    TAM_MAX_DEFAULT = 100
-    
-    def __init__(self, n=TAM_MAX_DEFAULT):
-        self.n = n 
-        self.m = 0 
-        self.adj = [[0.0 for i in range(n)] for j in range(n)]
+            # Escreve os rótulos dos vértices
+            for vertice in self.adj.keys():
+                arquivo.write(f"{vertice}\n")
 
-    def insereA(self, v, w, valor):
-        if self.adj[v][w] == 0 and self.adj[w][v] == 0: 
-            self.adj[v][w] = valor
-            self.adj[w][v] = valor 
-            self.m += 1 
-    
-    def removeA(self, v, w):
-        if self.adj[v][w] != 0 and self.adj[w][v] != 0: 
-            self.adj[v][w] = 0
-            self.adj[w][v] = 0 
-            self.m -= 1 
-    
-    def show(self):
-        print(f"\n n: {self.n:2d} ", end="")
-        print(f"m: {self.m:2d}\n")
-        for i in range(self.n):
-            for w in range(self.n):
-                print(f"Adj[{i:2d},{w:2d}] = {self.adj[i][w]} ", end="")
-            print("\n")
-        print("fim da impressao do grafo." )
-    
-    def showMin(self):
-        print(f"\n n: {self.n:2d} ", end="")
-        print(f"m: {self.m:2d}\n")
-        for i in range(self.n):
-            for w in range(self.n):
-                if self.adj[i][w] != 0:
-                    print(" 1 ", end="")
-                else:
-                    print(" 0 ", end="")
-            print("\n")
-        print("fim da impressao do grafo." )
+            # Escreve a quantidade de arestas
+            total_arestas = sum(len(vizinhos) for vizinhos in self.adj.values())
+            arquivo.write(f"{total_arestas}\n")
+
+            # Escreve as arestas e pesos
+            for vertice, vizinhos in self.adj.items():
+                for vizinho, peso in vizinhos.items():
+                    arquivo.write(f"{vertice} {vizinho} {peso}\n")
+
+        print("Dados salvos no arquivo com sucesso.")
+
--- main.py
+++ main.py
@@ -1,56 +1,100 @@
+import os
 from grafoMatriz import Grafo
 
 def menu():
-    opcao = -1
-    
+    print("=== RELAÇÃO DOS TIPOS DE POKÉMON EM GRAFO ===\n")
+    print("Selecione uma opção:")
+    print("[1] Ler dados do arquivo grafo.txt")
+    print("[2] Gravar dados no arquivo grafo.txt")
+    print("[3] Inserir vértice")
+    print("[4] Inserir aresta")
+    print("[5] Remover vértice")
+    print("[6] Remover aresta")
+    print("[7] Mostrar conteúdo do arquivo")
+    print("[8] Mostrar grafo")
+    print("[9] Apresentar a conexidade do grafo e o reduzido")
+    print("[0] Encerrar a aplicação")
+
+def main():
+    grafo = Grafo()
     while True:
-        print("""\n=== ANÁLISE EM GRAFO DOS TIPOS DE POKÉMON ===\n
-[1] Leitura do arquivo "grafo.txt"
-[2] Gravação de dados em "grafo.txt"
-[3] Inserir vértice
-[4] Inserir aresta
-[5] Remover vértice
-[6] Remover aresta
-[7] Mostrar conteúdo 
-[8] Conexidade e grafo reduzido
-[0] Encerrar""")
+        os.system('cls' if os.name == 'nt' else 'clear')
+        menu()
+        opcao = input("\nOpção: ")
         
-        opcao = input("\nOpção >> ")
+        # 1
+        if opcao == "1":
+            arquivo = input("Digite o nome do arquivo: ")
+            try:
+                grafo.lerArquivo(arquivo)
+                print(f"Dados do arquivo {arquivo} lidos com sucesso.")
+            except:
+                print(f"[ERRO] Não foi possível realizar a leitura do arquivo {arquivo}.")
+            input("\nPressione [ENTER] para voltar")
+        # 2
+        elif opcao == "2":
+            arquivo = input("Digite o nome do arquivo: ")
+            try:
+                grafo.salvarArquivo(arquivo)
+                print(f"Dados gravados no arquivo {arquivo} com sucesso.")
+            except:
+                print(f"Não foi possível gravar os dados no arquivo {arquivo}.")
+            input("\nPressione [ENTER] para voltar")
 
-        match opcao:
-            case "1":
-                # TODO
-                fileGraph = Grafo.fromFile("grafo.txt")
-                fileGraph.show()
+        # 3
+        elif opcao == "3":
+            vertice = input("Digite o nome do vértice: ")
+            grafo.insereV(vertice)
+            print(f"Vértice '{vertice}' inserido com sucesso.")
+            input("\nPressione [ENTER] para voltar")
+        
+        # 4
+        elif opcao == "4":
+            origem = input("Digite o vértice de origem: ")
+            destino = input("Digite o vértice de destino: ")
+            peso = float(input("Digite o peso da aresta: "))
+            grafo.insereA(origem, destino, peso)
+            print(f"Aresta de '{origem}' para '{destino}' com peso {peso} inserida com sucesso.")
+            input("\nPressione [ENTER] para voltar")
+        
+        # 5
+        elif opcao == "5":
+            vertice = input("Digite o nome do vértice a ser removido: ")
+            grafo.removeV(vertice)
+            input("\nPressione [ENTER] para voltar")
+        
+        # 6
+        elif opcao == "6":
+            origem = input("Digite o vértice de origem da aresta a ser removida: ")
+            destino = input("Digite o vértice de destino da aresta a ser removida: ")
+            grafo.removeA(origem, destino)
+            print(f"Aresta de '{origem}' para '{destino}' removida com sucesso.")
+            input("\nPressione [ENTER] para voltar")
+        
+        # 7
+        elif opcao == "7":
+            grafo.show()
+            input("\nPressione [ENTER] para voltar")
+        
+        # 8
+        elif opcao == "8":
+            grafo.showMin()
+            input("\nPressione [ENTER] para voltar")
+        
+        # 9
+        elif opcao == "9":
+            grafo.fconex()
+            input("\nPressione [ENTER] para voltar")
+        
+        # 0
+        elif opcao == "0":
+            print("\nEncerrando a aplicação...")
+            break
+        
+        # DEFAULT
+        else:
+            print("Opção inválida. Por favor, selecione uma opção válida.")
+            input("\nPressione [ENTER] para voltar")
 
-            # case 2:
-            #     # TODO 
-                
-            # case 3:
-            #     # TODO 
-                
-            # case 4:
-            #     # TODO 
-                
-            # case 5:
-            #     # TODO 
-                
-            # case 6:
-            #     # TODO 
-                
-            # case 7:
-            #     # TODO 
-                
-            # case 8:
-            #     # TODO
-                
-            case "0":
-                print("Encerrando programa")
-                break
-            case _:
-                # TODO default
-                print("Opção inválida")
-    
-    
-
-menu()
+if __name__ == "__main__":
+    main()
