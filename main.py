a5bb065808a40bf960cd93de7c708f1ac95d13e3
Tomás Fiorelli Barbosa <tomasfiorellibarbosa@icloud.com>
2024-05-21 17:34:14 -0300

    Ajustes na impressão do menu de navegação

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from grafo import Grafo
 def menu():
     print("=== RELAÇÃO DOS TIPOS DE POKÉMON EM GRAFO ===\n")
     print("Selecione uma opção:")
+    print("[1] Menu de edição")
+    print("[2] Menu de análises")
+    print("[0] Encerrar a aplicação")
+
+def menu_editor():
+    print("=== RELAÇÃO DOS TIPOS DE POKÉMON EM GRAFO ===\n")
+    print("Selecione uma opção:")
     print("[1] Ler dados de arquivo")
     print("[2] Gravar dados nem arquivo")
     print("[3] Inserir vértice")
@@ -13,82 +20,102 @@ def menu():
     print("[7] Mostrar conteúdo do arquivo")
     print("[8] Mostrar grafo")
     print("[9] Apresentar a conexidade do grafo e o reduzido")
-    print("[0] Encerrar a aplicação")
+    print("[0] <- Voltar")
 
 def main():
     grafo = Grafo()
-    while True:
+    flag = True
+    while flag:
         os.system('cls' if os.name == 'nt' else 'clear')
         menu()
         opcao = input("\nOpção: ")
         
-        # 1
-        if opcao == "1":
-            arquivo = input("Digite o nome do arquivo: ")
-            try:
-                grafo.ler_arquivo(arquivo)
-                print(f"\nDados do arquivo {arquivo} lidos com sucesso.")
-            except:
-                print(f"[ERRO] Não foi possível realizar a leitura do arquivo {arquivo}.")
-        
-        # 2
-        elif opcao == "2":
-            arquivo = input("Digite o nome do arquivo: ")
-            try:
-                grafo.salvar_arquivo(arquivo)
-                print(f"Dados gravados no arquivo {arquivo} com sucesso.")
-            except:
-                print(f"Não foi possível gravar os dados no arquivo {arquivo}.")
+        while opcao == "1":
+            os.system('cls' if os.name == 'nt' else 'clear')
+            menu_editor()
+            opcao = input("\nOpção: ")
+            
+            # 1
+            if opcao == "1":
+                arquivo = input("Digite o nome do arquivo: ")
+                try:
+                    grafo.ler_arquivo(arquivo)
+                    print(f"\nDados do arquivo {arquivo} lidos com sucesso.")
+                except:
+                    print(f"[ERRO] Não foi possível realizar a leitura do arquivo {arquivo}.")
+            
+            # 2
+            elif opcao == "2":
+                arquivo = input("Digite o nome do arquivo: ")
+                try:
+                    grafo.salvar_arquivo(arquivo)
+                    print(f"Dados gravados no arquivo {arquivo} com sucesso.")
+                except:
+                    print(f"Não foi possível gravar os dados no arquivo {arquivo}.")
 
-        # 3
-        elif opcao == "3":
-            vertice = input("Digite o nome do vértice: ")
-            grafo.insere_vertice(vertice)
-            print(f"Vértice '{vertice}' inserido com sucesso.")
+            # 3
+            elif opcao == "3":
+                vertice = input("Digite o nome do vértice: ")
+                grafo.insere_vertice(vertice)
+                print(f"Vértice '{vertice}' inserido com sucesso.")
+                input("\nPressione [ENTER] para voltar")
+            
+            # 4
+            elif opcao == "4":
+                origem = input("Digite o vértice de origem: ")
+                destino = input("Digite o vértice de destino: ")
+                peso = float(input("Digite o peso da aresta: "))
+                grafo.insere_aresta(origem, destino, peso)
+                print(f"Aresta de '{origem}' para '{destino}' com peso {peso} inserida com sucesso.")
+            
+            # 5
+            elif opcao == "5":
+                vertice = input("Digite o nome do vértice a ser removido: ")
+                grafo.remove_vertice(vertice)
+            
+            # 6
+            elif opcao == "6":
+                origem = input("Digite o vértice de origem da aresta a ser removida: ")
+                destino = input("Digite o vértice de destino da aresta a ser removida: ")
+                grafo.remove_aresta(origem, destino)
+                print(f"Aresta de '{origem}' para '{destino}' removida com sucesso.")
+            
+            # 7
+            elif opcao == "7":
+                grafo.mostrar()
+            
+            # 8
+            elif opcao == "8":
+                grafo.mostrar_minimo()
+            
+            # 9
+            elif opcao == "9":
+                grafo.fconex()
+            
+            # 0
+            elif opcao == "0":
+                opcao = None
+                break
+
+            # Default
+            else:
+                print("Opção inválida. Por favor, selecione uma opção válida.")
+            
             input("\nPressione [ENTER] para voltar")
+            opcao = None
         
-        # 4
-        elif opcao == "4":
-            origem = input("Digite o vértice de origem: ")
-            destino = input("Digite o vértice de destino: ")
-            peso = float(input("Digite o peso da aresta: "))
-            grafo.insere_aresta(origem, destino, peso)
-            print(f"Aresta de '{origem}' para '{destino}' com peso {peso} inserida com sucesso.")
-        
-        # 5
-        elif opcao == "5":
-            vertice = input("Digite o nome do vértice a ser removido: ")
-            grafo.remove_vertice(vertice)
-        
-        # 6
-        elif opcao == "6":
-            origem = input("Digite o vértice de origem da aresta a ser removida: ")
-            destino = input("Digite o vértice de destino da aresta a ser removida: ")
-            grafo.remove_aresta(origem, destino)
-            print(f"Aresta de '{origem}' para '{destino}' removida com sucesso.")
-        
-        # 7
-        elif opcao == "7":
-            grafo.mostrar()
-        
-        # 8
-        elif opcao == "8":
-            grafo.mostrar_minimo()
-        
-        # 9
-        elif opcao == "9":
-            grafo.fconex()
-        
-        # 0
-        elif opcao == "0":
+        if opcao == "0":
             print("\nEncerrando a aplicação...")
             break
-        
-        # DEFAULT
-        else:
+
+        # Default
+        elif opcao not in ["1", "2", "0", None]:
             print("Opção inválida. Por favor, selecione uma opção válida.")
         
-        input("\nPressione [ENTER] para voltar")
+        if opcao == None:
+            pass
+        else:
+            input("\nPressione [ENTER] para voltar")
 
 if __name__ == "__main__":
     main()
